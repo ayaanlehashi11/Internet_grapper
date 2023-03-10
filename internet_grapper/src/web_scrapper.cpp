1c452f6cb33ca37c390e93447f894bbba0e56023
Ayaanle hashi <91608871+ayaanlehashi11@users.noreply.github.com>
2023-03-10 12:56:24 +0300

    web-scraper using curl and gumbo

--- internet_grapper/src/web_scrapper.cpp
+++ internet_grapper/src/web_scrapper.cpp
@@ -7,89 +7,157 @@
 // Description : webscrapper in C++, Ansi-style
 //============================================================================
 
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <string>
+#include "iostream"
+#include "string"
+#include "algorithm"
+#include "curl/curl.h"
+#include "gumbo.h"
+#include "string.h"
 
-namespace beast = boost::beast;     // from <boost/beast.hpp>
-namespace http = beast::http;       // from <boost/beast/http.hpp>
-namespace net = boost::asio;        // from <boost/asio.hpp>
-using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+typedef size_t (*curl_write)(char *, size_t, size_t, std::string *);
 
-// Performs an HTTP GET and prints the response
-int main(int argc, char** argv)
+std::string request(std::string word)
 {
-    try
+  CURLcode res_code = CURLE_FAILED_INIT;
+  CURL *curl = curl_easy_init();
+  std::string result;
+  std::string url = "https://www.merriam-webster.com/dictionary/" + word;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  if (curl)
+  {
+    curl_easy_setopt(curl,
+                     CURLOPT_WRITEFUNCTION,
+                     static_cast<curl_write>([](char *contents, size_t size,
+                                                size_t nmemb, std::string *data) -> size_t
+                                             { 
+                                               size_t new_size = size * nmemb;
+                                               if (data == NULL) {
+                                                 return 0;
+                                               } 
+                                               data->append(contents, new_size);
+                                               return new_size; }));
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "simple scraper");
+
+    res_code = curl_easy_perform(curl);
+
+    if (res_code != CURLE_OK)
     {
-        // Check command line arguments.
-        if(argc != 4 && argc != 5)
-        {
-            std::cerr <<
-                "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
-                "Example:\n" <<
-                "    http-client-sync www.example.com 80 /\n" <<
-                "    http-client-sync www.example.com 80 / 1.0\n";
-            return EXIT_FAILURE;
-        }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const target = argv[3];
-        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
-
-        // The io_context is required for all I/O
-        net::io_context ioc;
-
-        // These objects perform our I/O
-        tcp::resolver resolver(ioc);
-        beast::tcp_stream stream(ioc);
-
-        // Look up the domain name
-        auto const results = resolver.resolve(host, port);
-
-        // Make the connection on the IP address we get from a lookup
-        stream.connect(results);
-
-        // Set up an HTTP GET request message
-        http::request<http::string_body> req{http::verb::get, target, version};
-        req.set(http::field::host, host);
-        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-        // Send the HTTP request to the remote host
-        http::write(stream, req);
-
-        // This buffer is used for reading and must be persisted
-        beast::flat_buffer buffer;
-
-        // Declare a container to hold the response
-        http::response<http::dynamic_body> res;
-
-        // Receive the HTTP response
-        http::read(stream, buffer, res);
-
-        // Write the message to standard out
-        std::cout << res << std::endl;
-
-        // Gracefully close the socket
-        beast::error_code ec;
-        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
-
-        // not_connected happens sometimes
-        // so don't bother reporting it.
-        //
-        if(ec && ec != beast::errc::not_connected)
-            throw beast::system_error{ec};
-
-        // If we get here then the connection is closed gracefully
+      return curl_easy_strerror(res_code);
     }
-    catch(std::exception const& e)
+
+    curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+
+  return result;
+}
+
+std::string str_replace(std::string search, std::string replace, std::string &subject)
+{
+  size_t count;
+  for (std::string::size_type pos{};
+       subject.npos != (pos = subject.find(search.data(), pos, search.length()));
+       pos += replace.length(), ++count)
+  {
+    subject.replace(pos, search.length(), replace.data(), replace.length());
+  }
+
+  return subject;
+}
+
+std::string strtolower(std::string str)
+{
+  std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+  return str;
+}
+
+std::string extract_text(GumboNode *node)
+{
+  if (node->type == GUMBO_NODE_TEXT)
+  {
+    return std::string(node->v.text.text);
+  }
+  else if (node->type == GUMBO_NODE_ELEMENT &&
+           node->v.element.tag != GUMBO_TAG_SCRIPT &&
+           node->v.element.tag != GUMBO_TAG_STYLE)
+  {
+    std::string contents = "";
+    GumboVector *children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; ++i)
     {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+      std::string text = extract_text((GumboNode *)children->data[i]);
+      if (i != 0 && !text.empty())
+      {
+        contents.append("");
+      }
+
+      contents.append(str_replace(":", ">", text));
     }
-    return EXIT_SUCCESS;
+
+    return contents;
+  }
+  else
+  {
+    return "";
+  }
+}
+
+std::string find_definitions(GumboNode *node)
+{
+  std::string res = "";
+  GumboAttribute *attr;
+  if (node->type != GUMBO_NODE_ELEMENT)
+  {
+    return res;
+  }
+
+  if ((attr = gumbo_get_attribute(&node->v.element.attributes, "class")) &&
+      strstr(attr->value, "dtText") != NULL)
+  {
+    res += extract_text(node);
+    res += "\n";
+  }
+
+  GumboVector *children = &node->v.element.children;
+  for (int i = 0; i < children->length; ++i)
+  {
+    res += find_definitions(static_cast<GumboNode *>(children->data[i]));
+  }
+
+  return res;
+}
+
+std::string scrape(std::string markup)
+{
+  std::string res = "";
+  GumboOutput *output = gumbo_parse_with_options(&kGumboDefaultOptions,
+                                                 markup.data(), markup.length());
+
+  res += find_definitions(output->root);
+
+  gumbo_destroy_output(&kGumboDefaultOptions, output);
+
+  return res;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cout << "Please provide a valid English word" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::string arg = argv[1];
+
+  std::string res = request(arg);
+  std::cout << scrape(res) << std::endl;
+
+  return EXIT_SUCCESS;
 }
